7e128e5f4f651cb171c4b2ad202b31e61591fdfb
apautvein <apautvein@gmail.com>
2022-05-20 21:48:15 -0500

    update javascript calculator to use React Hooks.

--- src/App.js
+++ src/App.js
@@ -1,157 +1,172 @@
 import './App.css';
 import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
-const [input, setInput] = useState("0"); 
-const [output, setOutput] = useState(""); 
-const [buffer, setBuffer] = useState(""); 
-const operators = ["+", "-", "*", "/"];
+  const [input, setInput] = useState("0");
+  const [output, setOutput] = useState("");
+  const [buffer, setBuffer] = useState("");
+  const operators = ["+", "-", "*", "/"];
 
-  const handleNumberClick = ({target: {value}}) => {
-    setInput(() => {
-      checkInput(value, input); 
-    }); 
+  const handleNumberClick = ({ target: { value } }) => {
+    setInput(() =>
+      checkInput(value, input)
+    );
 
-    setBuffer(() => {
-      checkInput(value, buffer); 
-    });
+    setBuffer(() =>
+      checkInput(value, buffer)
+    );
   }
 
   const checkInput = (value, current) => {
     if (input === "0") {
       return `${value}`;
     } else if (operators.some((oper) => oper === input)) {
-      return current += value;
+      return current + value;
     }
-    return current += value; 
-  }; 
+    return current + value;
+  };
 
-  handleClearClick(clear) {
-    setState({ input: "0", output: "", buffer: "" });
+  const handleClearClick = () => {
+    setInput(() => "0");
+
+    setBuffer(() => "");
+
+    setOutput(() => "");
   }
 
-  handleOperatorClick(e) {
-    const { value } = e.target;
-    const containsOperator = /[\*\-\+\/]/.test(state.input);
-    const endsWithMinus = /\-$/.test(state.input);
-    if (state.output) {
-      setState((state) => {
-        return { input: value, buffer: (state.output += value), output: "" };
-      });
+  const handleOperatorClick = ({ target: { value } }) => {
+    const containsOperator = /[*\-+/]/.test(input);
+    const endsWithMinus = /-$/.test(input);
+
+    if (output) {
+
+      setInput(() => value);
+
+      setBuffer(() => output + value);
+
+      setOutput(() => "");
+
     } else if (!containsOperator) {
-      setState((state) => {
-        return { input: value, buffer: (state.buffer += value) };
-      });
+
+      setInput(() => value);
+
+      setBuffer(() => buffer + value);
+
     } else if (value === "-" && !endsWithMinus) {
-      setState((state) => {
-        return { input: value, buffer: (state.buffer += value) };
-      });
+
+      setInput(() => value);
+
+      setBuffer(() => buffer + value);
     } else {
-      let number = state.buffer.replace(/\D+$/, "");
-      setState((state) => {
-        return { input: value, buffer: (number += value) };
-      });
+
+      let number = buffer.replace(/\D+$/, "");
+
+      setInput(() => value);
+
+      setBuffer(() => (number + value));
     }
   }
 
-  handleEqualsClick() {
-    const lastCharacter = state.buffer[state.buffer.length - 1];
+  const handleEqualsClick = () => {
+    const lastCharacter = buffer[buffer.length - 1];
 
     if (/[0-9]/.test(lastCharacter)) {
-      setState((state) => {
-        return {
-          output: math.evaluate(state.buffer),
-          input: math.evaluate(state.buffer),
-          buffer: (state.buffer += "=")
-        };
-      });
+      setInput(() => evaluate(buffer));
+
+      setBuffer(() => (buffer + "="));
+
+      setOutput(() => evaluate(buffer));
     } else {
-      const split = removeLastCharacter(state.buffer);
-      setState({
-        output: math.evaluate(split),
-        input: math.evaluate(split),
-        buffer: (split += "=")
-      });
+      const split = removeLastCharacter(buffer);
+
+      setInput(() => evaluate(split));
+
+      setBuffer(() => (split + "="));
+
+      setOutput(() => evaluate(split));
     }
   }
 
-  handleDecimalClick() {
-    if (!/\.$/.test(state.input) && !/\.\d*/.test(state.input)) {
-      setState((state) => {
-        return { buffer: (state.buffer += "."), input: (state.input += ".") };
-      });
+  const handleDecimalClick = () => {
+    if (!/\.$/.test(input) && !/\.\d*/.test(input)) {
+      setInput(() => (input + "."));
+
+      setBuffer(() => (buffer + "."));
     }
   }
+
   const removeLastCharacter = (string) => {
     let split = string.split("");
     split.pop();
     return split.join("");
   };
+
   return (
     <div id="container">
-          <div id="calculator">
-            <div id="input-area">
-              <p>
-                {state.buffer}
-                {state.output}
-              </p>
-              <div id="display">{state.input}</div>
-            </div>
-            <div id="buttons">
-              <button id="clear" onClick={handleClearClick}>
-                Clear
-              </button>
-              <button id="divide" value="/" onClick={handleOperatorClick}>
-                /
-              </button>
-              <button id="seven" value="7" onClick={handleNumberClick}>
-                7
-              </button>
-              <button id="eight" value="8" onClick={handleNumberClick}>
-                8
-              </button>
-              <button id="nine" value="9" onClick={handleNumberClick}>
-                9
-              </button>
-              <button id="multiply" value="*" onClick={handleOperatorClick}>
-                *
-              </button>
-              <button id="four" value="4" onClick={handleNumberClick}>
-                4
-              </button>
-              <button id="five" value="5" onClick={handleNumberClick}>
-                5
-              </button>
-              <button id="six" value="6" onClick={handleNumberClick}>
-                6
-              </button>
-              <button id="subtract" value="-" onClick={handleOperatorClick}>
-                -
-              </button>
-              <button id="one" value="1" onClick={handleNumberClick}>
-                1
-              </button>
-              <button id="two" value="2" onClick={handleNumberClick}>
-                2
-              </button>
-              <button id="three" value="3" onClick={handleNumberClick}>
-                3
-              </button>
-              <button id="add" value="+" onClick={handleOperatorClick}>
-                +
-              </button>
-              <button id="zero" value="0" onClick={handleNumberClick}>
-                0
-              </button>
-              <button id="decimal" onClick={handleDecimalClick}>
-                .
-              </button>
-              <button id="equals" onClick={handleEqualsClick}>
-                =
-              </button>
-            </div>
-          </div>
+      <div id="calculator">
+        <div id="input-area">
+          <p>
+            {buffer}
+            {output}
+          </p>
+          <div id="display">{input}</div>
         </div>
+        <div id="buttons">
+          <button id="clear" onClick={handleClearClick}>
+            Clear
+          </button>
+          <button id="divide" value="/" onClick={handleOperatorClick}>
+            /
+          </button>
+          <button id="seven" value="7" onClick={handleNumberClick}>
+            7
+          </button>
+          <button id="eight" value="8" onClick={handleNumberClick}>
+            8
+          </button>
+          <button id="nine" value="9" onClick={handleNumberClick}>
+            9
+          </button>
+          <button id="multiply" value="*" onClick={handleOperatorClick}>
+            *
+          </button>
+          <button id="four" value="4" onClick={handleNumberClick}>
+            4
+          </button>
+          <button id="five" value="5" onClick={handleNumberClick}>
+            5
+          </button>
+          <button id="six" value="6" onClick={handleNumberClick}>
+            6
+          </button>
+          <button id="subtract" value="-" onClick={handleOperatorClick}>
+            -
+          </button>
+          <button id="one" value="1" onClick={handleNumberClick}>
+            1
+          </button>
+          <button id="two" value="2" onClick={handleNumberClick}>
+            2
+          </button>
+          <button id="three" value="3" onClick={handleNumberClick}>
+            3
+          </button>
+          <button id="add" value="+" onClick={handleOperatorClick}>
+            +
+          </button>
+          <button id="zero" value="0" onClick={handleNumberClick}>
+            0
+          </button>
+          <button id="decimal" onClick={handleDecimalClick}>
+            .
+          </button>
+          <button id="equals" onClick={handleEqualsClick}>
+            =
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
